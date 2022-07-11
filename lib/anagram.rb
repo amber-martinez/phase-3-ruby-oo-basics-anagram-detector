89a73e6d6aed3fb35ae5faeecd262ea587245015
Amber <martinez.amber1028@gmail.com>
2022-07-11 16:21:37 -0700

    commit tree 510fb88b09f68416f63e66306ed11c90125aa9f5

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
-# Your code goes here!
+class Anagram
+    
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(match)
+        @match = match
+
+        matches = @match.select do |word|
+            if word.chars.sort == @word.chars.sort
+                word
+            end
+        end
+
+        matches
+
+    end
+
+
+end
+
+listen = Anagram.new("cinema")
+listen.match(%w[iceman books anime yes])
